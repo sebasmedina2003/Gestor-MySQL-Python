f3ebffaa21808c432a459b5555373e6bce00f6b5
Sebastián Medina <sebasmedina2003@gmail.com>
2023-04-24 14:59:35 -0400

    Modificacion del README y finalizado los inserts

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center"> Manejador de consultas para MySQL server </h1>
+
+<p>Lista de librerias usadas</p>
+
+<li>
+    <ul>mysql-connector-python</ul>
+</li>
--- manejador_db.py
+++ manejador_db.py
@@ -26,8 +26,9 @@ class manejadorDB:
         """
         self.cursor.execute(f"COMMIT;")
 
-    def insertWhereAND(self, table: str, listColumns: list, listValues: list, listParameters: list, listValuesParameters: str) -> None:
+    def insertColumnsWhereAND(self, table: str, listColumns: list, listValues: list, listParameters: list, listValuesParameters: list) -> None:
         """
+        INSERT INTO table(listColumns) VALUES(listValues) WHERE listParameters = listValuesParameters AND... ;\n
         Atraves del parametro listColumns se le pasa el nombre de las columnas de la base de datos donde se van a insertar los valores,
         la listValues corresponde a los valores que se van a insertar en listColumns, la listParameters son la lista de los parametros
         a usar para la insercion de datos y la listValuesParameters son la lista de valores correspondientes a la lista de parametros.
@@ -54,8 +55,49 @@ class manejadorDB:
         self.cursor.execute(query)
         self.commit()
 
+    def insertColumnsWhereOR(self, table: str, listColumns: list, listValues: list, listParameters: list, listValuesParameters: list) -> None:
+        """
+        INSERT INTO table(listColumns) VALUES(listValues) WHERE listParameters = listValuesParameters OR... ;\n
+        Atraves del parametro listColumns se le pasa el nombre de las columnas de la base de datos donde se van a insertar los valores,
+        la listValues corresponde a los valores que se van a insertar en listColumns, la listParameters son la lista de los parametros
+        a usar para la insercion de datos y la listValuesParameters son la lista de valores correspondientes a la lista de parametros.
+        Para unir los parametros este metodo usa la sentencia logica OR
+        """
+        columns = ""
+        values = ""
+        parameters = ""
 
-prueba = manejadorDB(user="root", password="root", host="localhost")
-prueba.useDataBase(nameDB="admin001000")
-prueba.insertWhereAND(table="articulo", listColumns=["hola", "prueba"], listValues=[
-                      "holaValue", "pruebaValue"], listParameters=["a"], listValuesParameters=["a"])
+        for columnas, valores in zip(listColumns, listValues):
+            columns += f"{columnas}, "
+            values += f"{valores}, "
+        columns = columns[0:len(columns)-2]
+        values = values[0:len(values)-2]
+
+        for parametros, valoresParametros in zip(listParameters, listValuesParameters):
+            parameters += f"{parametros}=\'{valoresParametros}\' OR "
+        parameters = parameters[0:len(parameters)-4]
+        parameters += ";"
+
+        query = f"INSERT INTO {table}({columns}) VALUES({values}) WHERE {parameters}"
+        self.cursor.execute(query)
+        self.commit()
+
+    def insertRow(self, table: str, listColumns: list, listValues: list) -> None:
+        """
+        INSERT INTO table(listColumns) VALUES(listValues);\n
+        Se debe pasar a traves del parametro listColumns una lista de strings que contengan el nombre de las columnas
+        donde se va a insertar la informacion pasada a traves del parametro listValues
+        """
+        columns = ""
+        values = ""
+
+        for columnas, valores in zip(listColumns, listValues):
+            columns += f"{columnas}, "
+            values += f"{valores}, "
+
+        columns = columns[0:len(columns)-2]
+        values = values[0:len(values)-2]
+
+        query = f"INSERT INTO {table}({columns}) VALUES({values})"
+        self.cursor.execute(query)
+        self.commit()
